d0c945356a02210aa532b9c1883ff5f39b3d0658
yashpatel5400 <ypatel@princeton.edu>
2016-04-03 04:41:11 -0400

    Did mapping for each of the diseases rather than just one

--- main.py
+++ main.py
@@ -51,47 +51,61 @@ def read_cache(filename):
 
 # Google Maps plot + options
 styles = json.dumps([{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"color":"#000000"},{"lightness":13}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#144b53"},{"lightness":14},{"weight":1.4}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#08304b"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#0c4152"},{"lightness":5}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#0b434f"},{"lightness":25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#0b3d51"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"}]},{"featureType":"transit","elementType":"all","stylers":[{"color":"#146474"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#021019"}]}])
-map_options = GMapOptions(lat=15, lng=0, map_type="roadmap", zoom=2, styles=styles)
+map_options = GMapOptions(lat=15, lng=0, map_type="roadmap",
+    zoom=2, styles=styles)
 plot = GMapPlot(
-    x_range=DataRange1d(), y_range=DataRange1d(), map_options=map_options, title="", plot_width=1200, plot_height=600, title_text_color={"value": "#ffffff"}
+    x_range=DataRange1d(), y_range=DataRange1d(), map_options=map_options, 
+    title="", plot_width=1200, plot_height=600, title_text_color={"value": "#ffffff"}
 )
 
 # Read in HIV data
-hivData = readInData('data/hiv.csv')
+color_disease = {
+    "hiv": "red",
+    #"leprosy": "yellow",
+    #"malaria": "teal",
+    "measles": "blue",
+    #"mumps": "pink",
+    #"tuberculosis": "purple"
+}
 
-# Find latitude and longitude of countries
-lats = []
-lons = []
-if not os.path.exists("./lats_cache.csv"):
-    for c in hivData['countries']:
-        r = requests.get("http://open.mapquestapi.com/nominatim/v1/search.php?key=5cMOU7Rg4xRCH27pndm8YalfVXG1Rc4P&format=json&q=" + c)
-        if (len(r.json()) > 0):
-            lats.append(r.json()[0]['lat'])
-            lons.append(r.json()[0]['lon'])
-        else:
-            print(c) # this shouldn't occur, so fix it when it does
-    write_cache("lats_cache.csv", lats)
-    write_cache("lons_cache.csv", lons)
-else:
-    lats = read_cache("lats_cache.csv")
-    lons = read_cache("lons_cache.csv")
+for disease in color_disease:
+    disease_data = readInData('./data/{}.csv'.format(disease))
 
-# Data source
-# the year should come from the slider
-# the cases should come from the disease data
-source = ColumnDataSource(data=dict(lat=lats, lon=lons, country=hivData['countries']))
-# source = ColumnDataSource(data=dict(lat=lats, lon=lons, title="HIV", country="", year=2013, cases=hivData['2013']))
+    # Find latitude and longitude of countries
+    lats = []
+    lons = []
+    if not os.path.exists("./{}_lats_cache.csv".format(disease)):
+        for c in disease_data['countries']:
+            r = requests.get("http://open.mapquestapi.com/nominatim/v1/search.php?key=5cMOU7Rg4xRCH27pndm8YalfVXG1Rc4P&format=json&q=" + c)
+            if (len(r.json()) > 0):
+                lats.append(r.json()[0]['lat'])
+                lons.append(r.json()[0]['lon'])
+            else:
+                print(c) # this shouldn't occur, so fix it when it does
+        write_cache("./{}_lats_cache.csv".format(disease), lats)
+        write_cache("./{}_lons_cache.csv".format(disease), lons)
+    else:
+        lats = read_cache("./{}_lats_cache.csv".format(disease))
+        lons = read_cache("./{}_lons_cache.csv".format(disease))
 
-# Add circles + hover
-hover = HoverTool(tooltips=[
-    ("Country","@country")
-])
-# hover = HoverTool(tooltips=[
-#     ("Country", "@country"),
-#     ("# of Cases", "@cases")
-# ])
-circle = Circle(x="lon", y="lat", size=20, fill_color="red", fill_alpha=0.7, line_color=None)
-plot.add_glyph(source, circle)
+    # Data source
+    # the year should come from the slider
+    # the cases should come from the disease data
+    source = ColumnDataSource(data=dict(lat=lats, lon=lons, 
+        country=disease_data['countries']))
+    # source = ColumnDataSource(data=dict(lat=lats, lon=lons, title="HIV", country="", year=2013, cases=disease_data['2013']))
+
+    # Add circles + hover
+    hover = HoverTool(tooltips=[
+        ("Country","@country")
+    ])
+    # hover = HoverTool(tooltips=[
+    #     ("Country", "@country"),
+    #     ("# of Cases", "@cases")
+    # ])
+    circle = Circle(x="lon", y="lat", size=20, 
+        fill_color=color_disease[disease], fill_alpha=0.25, line_color=None)
+    plot.add_glyph(source, circle)
 
 # for i in range(0, len(locations)):
 #     # Data source
@@ -106,13 +120,13 @@ plot.add_tools(PanTool(), WheelZoomTool(), hover)
 # callback = CustomJS(args=dict(source=source), code="""
 #         var data = source.get('data');
 #         var year = cb_obj.get('value');
-#         data['cases'] = hivData[str(year)];
+#         data['cases'] = disease_data[str(year)];
 #         source.trigger('change');
 #     """)
 # slider = HBox(Slider(start=2000, end=2016, value=2016, step=1, title="Year", callback=callback), width=300
-slider = HBox(Slider(start=2000, end=2016, value=2016, step=1, title="Year"), width=300)
+slider = Slider(start=2000, end=2016, value=2016, step=1, title="Year")
 
 # Output the plot and show it
 output_file("disease_visual.html")
-layout = vform(slider, plot)
+layout = vform(plot, slider)
 show(layout)
