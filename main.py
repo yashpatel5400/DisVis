df468d17490e81075717b30e41e68ad5105d497b
Richard Chu <richardchu10@gmail.com>
2016-04-03 04:36:23 -0400

    Add cases

--- main.py
+++ main.py
@@ -20,14 +20,16 @@ def readInData(csvString):
 
     countries = []
     for year in years:
-        dataDict[year] = {}
+        dataDict[year] = []
     for row in rows[1:]:
         country = row[0]
         countries.append(country)
         for year in range(2000, 2017):
-            dataDict[str(year)][country] = 0
-        for i in range(1, len(row)):
-            dataDict[rows[0][i]][country] = row[i]
+            if (rows[0].count(str(year)) > 0):
+                i = rows[0].index(str(year))
+                dataDict[str(year)].append(row[i])
+            else:
+                dataDict[str(year)].append(0)
         
     dataDict['countries'] = countries
     dataDict['years'] = years
@@ -79,17 +81,14 @@ else:
 # Data source
 # the year should come from the slider
 # the cases should come from the disease data
-source = ColumnDataSource(data=dict(lat=lats, lon=lons, country=hivData['countries']))
+source = ColumnDataSource(data=dict(lat=lats, lon=lons, country=hivData['countries'], cases=hivData['2013']))
 # source = ColumnDataSource(data=dict(lat=lats, lon=lons, title="HIV", country="", year=2013, cases=hivData['2013']))
 
 # Add circles + hover
 hover = HoverTool(tooltips=[
-    ("Country","@country")
+    ("Country","@country"),
+    ("# of Cases", "@cases")
 ])
-# hover = HoverTool(tooltips=[
-#     ("Country", "@country"),
-#     ("# of Cases", "@cases")
-# ])
 circle = Circle(x="lon", y="lat", size=20, fill_color="red", fill_alpha=0.7, line_color=None)
 plot.add_glyph(source, circle)
 
@@ -101,16 +100,21 @@ plot.add_glyph(source, circle)
 #     circle = Circle(x="lon", y="lat", size=20, fill_color="red", fill_alpha=0.7, line_color=None)
 #     plot.add_glyph(source, circle)
 
+# def update(attrname, old, new):
+#     source.data.cases = hivData[str(new)]
+#     print(attrname, old, new)
+
 # Add plot options + slider
 plot.add_tools(PanTool(), WheelZoomTool(), hover)
 # callback = CustomJS(args=dict(source=source), code="""
 #         var data = source.get('data');
 #         var year = cb_obj.get('value');
-#         data['cases'] = hivData[str(year)];
+#         data['cases'] = 
 #         source.trigger('change');
 #     """)
 # slider = HBox(Slider(start=2000, end=2016, value=2016, step=1, title="Year", callback=callback), width=300
-slider = HBox(Slider(start=2000, end=2016, value=2016, step=1, title="Year"), width=300)
+slider = HBox(Slider(start=2000, end=2016, value=2013, step=1, title="Year"), width=300)
+# slider.on_change('value', update)
 
 # Output the plot and show it
 output_file("disease_visual.html")
